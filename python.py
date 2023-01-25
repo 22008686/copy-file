8ba445be4dbf925071179ac51c28c9ef84923985
22008686 <pm996299@gmail.com>
2023-01-25 19:30:22 +0530

    added

---
+++ python.py
@@ -0,0 +1,6 @@
+##Developed by:M.PAVITHRA
+##Reference number:22008686
+#with open('lines.txt','r') as file1:
+    with open('text.txt','w') as file2:
+        for line in file1:
+            file2.write(line)
